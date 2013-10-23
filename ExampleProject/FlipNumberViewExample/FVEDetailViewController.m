c0da50b3ca5c57b6a3189ac7e91b9b48bdecc948
Markus Emrich <Markus.Emrich@gmail.com>
2013-10-23 13:36:06 +0200

    fixed layout for ios7

--- ExampleProject/FlipNumberViewExample/FVEDetailViewController.m
+++ ExampleProject/FlipNumberViewExample/FVEDetailViewController.m
@@ -196,9 +196,14 @@ static CGFloat const FVEDetailControllerTargetedViewTag = 111;
         return;
     }
     
+    CGFloat multiplier = 0.9;
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
+        multiplier = 1.0;
+    }
+    
     self.flipView.frame = CGRectInset(self.view.bounds, 20, 20);
     self.flipView.center = CGPointMake(floor(self.view.frame.size.width/2),
-                                  floor((self.view.frame.size.height/2)*0.9));
+                                  floor((self.view.frame.size.height/2)*multiplier));
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
